41d9e5f27c1b61a98682309e421a68c932c6cb40
sarapoza <sara.saldanha.poza@gmail.com>
2020-07-19 14:44:44 -0300

    implementacao calc soma

---
+++ src/main/java/br/com/pozasar/MathController.java
@@ -0,0 +1,40 @@
+package br.com.pozasar;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+//	CÓDIGOS COMENTADOS REFEREM A PRIMEIRA VERSÃO DO PROJETO
+//	private static final String template = "Hello, %s!";
+//	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+//	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+//	}
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
+		return sum;
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if(strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if(strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
